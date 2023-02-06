28d6f93dd6721622c74067db1d9e475329a15966
Alexandra <gosteli.alexandra@gmail.com>
2023-02-06 22:10:22 +0100

    task52

--- Program.cs
+++ Program.cs
@@ -28,41 +28,50 @@
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 //  и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.Write("Введите количество строк: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите строку числа которое хотите найти: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите столбец числа которое хотите найти: ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите строку числа которое хотите найти: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите столбец числа которое хотите найти: ");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = new int[m, n];
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        array[i, j] = new Random().Next(0, 20);
-        Console.Write($"{array[i, j]} ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-if (M <= m && N <= n && M > 0 && N > 0)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (i == M - 1 && j == N - 1)
-            {
-                Console.WriteLine($"Число на строке {M} в столбце {N} -->  {array[i, j]} ");
-            }
-            continue;
-        }
-    }
-}
-else
-{
-    Console.WriteLine($"Числа на строке {M} в столбце {N} НЕТ ");
-}
+// int[,] array = new int[m, n];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(0, 20);
+//         Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine();
+// if (M <= m && N <= n && M > 0 && N > 0)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (i == M - 1 && j == N - 1)
+//             {
+//                 Console.WriteLine($"Число на строке {M} в столбце {N} -->  {array[i, j]} ");
+//             }
+//             continue;
+//         }
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"Числа на строке {M} в столбце {N} НЕТ ");
+// }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
