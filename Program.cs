bd2eb133ea35a2187c876d392e2cdc7977c2c7d1
Alexandra <gosteli.alexandra@gmail.com>
2023-02-06 22:06:09 +0100

    task47 done

--- Program.cs
+++ Program.cs
@@ -7,3 +7,20 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] array = new double[m, n];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.NextDouble() * 10;
+        Console.Write($"{Math.Round(array[i, j],2)} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
